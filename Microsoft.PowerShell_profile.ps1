08fa8adac4e2c3bddb15a0be0dcdede4f27f150c
Vishal Mamidi <mamidivishalsa@gmail.com>
2023-08-02 15:12:43 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,12 @@
+# functions
+function FuntFP {
+    terraform plan --var-file="environment/uat.tfvars" -out="terraform.tfplan"
+}
+function FunVis {
+    echo "he is the great persion in the world"
+}
+
 # Create aliases for frequently used commands
 Set-Alias tf terraform
+Set-Alias -Name tfp -Value FuntFP
+Set-Alias -Name vishal -Value FunVis
